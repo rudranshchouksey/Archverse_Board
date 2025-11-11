8cf3bbf01d04ed7552c55bf3286c128de12dbf62
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2025-11-11 23:45:55 +0530

    members route

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -42,6 +42,9 @@ export async function createAdminClient() {
     return {
         get account() {
             return new Account(client)
+        },
+        get users() {
+            return new Users(client)
         }
     }    
 
