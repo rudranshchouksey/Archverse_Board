84e8f2af6bf8c39b04e5178f77e6fe1b4ec54216
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2025-11-11 12:04:23 +0530

    standlone layout

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -24,6 +24,7 @@ import { Button } from "@/components/ui/button";
 import { useCreateWorkspace } from "../api/use-create-workspace";
 import { ImageIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 interface CreateWorkspaceFormProps {
     onCancel?: () => void;
@@ -159,6 +160,7 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                                     variant={"secondary"}
                                     onClick={onCancel}
                                     disabled={isPending}
+                                    className={cn(!onCancel && "invisible" )}
                                 >
                                     Cancel
                                 </Button>
