64e3aeb72107cd210a50ee56cb8650546e78b525
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2025-11-18 19:05:16 +0530

    Calendar Page Completed

--- src/features/tasks/components/data-calender.tsx
+++ src/features/tasks/components/data-calender.tsx
@@ -14,6 +14,9 @@ import { useState } from "react";
 
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import "./data-calendar.css"
+import { EventCard } from "./event-card";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
 const locales = {
     "en-US": enUS
@@ -29,6 +32,36 @@ const localizer = dateFnsLocalizer({
 
 interface DataCalendarProps {
     data: Task[]
+}
+
+interface CustomToolbarProps {
+    date: Date;
+    onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+}
+
+const CustomToolbar = ({ date, onNavigate }: CustomToolbarProps) => {
+   return (
+    <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+        <Button
+            onClick={() => onNavigate("PREV")}
+            variant="secondary"
+            size="icon"
+        >
+            <ChevronLeftIcon className="size-4" />
+        </Button>
+        <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+            <CalendarIcon className="size-4 mr-2" />
+            <p className="text-sm">{format(date, "MMMM yyyy")}</p>
+        </div>
+        <Button
+            onClick={() => onNavigate("NEXT")}
+            variant="secondary"
+            size="icon"
+        >
+            <ChevronRightIcon className="size-4" />
+        </Button>
+    </div>
+   ) 
 }
 
 export const DataCalendar = ({
@@ -71,6 +104,20 @@ export const DataCalendar = ({
             max={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
             formats={{
                 weekdayFormat: (date, culture, localizer) => localizer?.format(date, "EEE", culture) ?? ""
+            }}
+            components={{
+                eventWrapper: ({ event }) => (
+                    <EventCard 
+                        id={event.id}
+                        title={event.title}
+                        assignee={event.assignee}
+                        project={event.project}
+                        status={event.status}
+                    />
+                ),
+                toolbar: () => (
+                    <CustomToolbar date={value} onNavigate={handleNavigate} />
+                )
             }}
         />
     )
