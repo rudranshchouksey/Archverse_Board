a2a1675d81257bb7eca4a8d2acbc397859e06db7
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2025-11-19 13:52:12 +0530

    Project analytics route setup

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -4,6 +4,7 @@ import { PageError } from "@/components/page-error"
 import { PageLoader } from "@/components/page-loader"
 import { Button } from "@/components/ui/button"
 import { useGetProject } from "@/features/projects/api/use-get-project"
+import { useGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics"
 import { ProjectAvatar } from "@/features/projects/components/project-avatar"
 import { useProjectId } from "@/features/projects/hook/use-project-id"
 import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher"
@@ -12,13 +13,16 @@ import Link from "next/link"
 
 export const ProjectIdClient = () => {
     const projectId = useProjectId()
-    const { data, isLoading } = useGetProject({ projectId })
+    const { data: project, isLoading: isLoadingProject } = useGetProject({ projectId })
+    const { data: analytics, isLoading: isLoadingAnalytics } = useGetProjectAnalytics({ projectId })
+
+    const isLoading = isLoadingAnalytics || isLoadingProject
 
     if (isLoading) {
         return <PageLoader />
     }
 
-    if (!data) {
+    if (!project || !analytics) {
         return <PageError message="Project not found" />
     }
 
@@ -27,15 +31,15 @@ export const ProjectIdClient = () => {
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-x-2">
                     <ProjectAvatar 
-                        name={data.name}
-                        image={data.imageUrl}
+                        name={project.name}
+                        image={project.imageUrl}
                         className="size-8"
                     />
-                    <p className="text-lg font-semibold">{data.name}</p>
+                    <p className="text-lg font-semibold">{project.name}</p>
                 </div>
                 <div>
                     <Button variant={"secondary"} size={"sm"} asChild >
-                        <Link href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}>
+                        <Link href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}>
                             <PencilIcon className="size-4 mr-2" />
                             Edit Project
                         </Link>
---
+++ src/features/projects/api/use-get-project-analytics.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { client } from "@/lib/rpc";
+
+interface useGetProjectAnalyticsProps {
+    projectId: string
+}
+
+export const useGetProjectAnalytics = ({
+    projectId
+}: useGetProjectAnalyticsProps) => {
+    const query = useQuery({
+        queryKey: ["projects-analytics", projectId],
+        queryFn: async () => {
+            const response = await client.api.projects[":projectId"]["analytics"].$get({
+                param: { projectId }
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch project analytics");
+            }
+
+            const { data } = await response.json();
+
+            return data;
+        },
+    });
+
+    return query
+}
